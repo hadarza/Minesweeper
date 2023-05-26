a6e062ca4c46ddbd9aaa01547d67423d831cc64b
hadar <hadar.zaguri@gmail.com>
2023-05-26 11:19:22 +0300

    adding useReducer - remove matrix + grid easier to implmentation

--- src/components/DashBoard/Board/Board.jsx
+++ src/components/DashBoard/Board/Board.jsx
@@ -1,34 +1,72 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState,useEffect,useReducer } from 'react';
 import { useSelector,useDispatch } from 'react-redux';
 import {setGameStart} from '../../../features/DashBoard/DashBoardSlice'
 import Cell from '../Cell/Cell';
 
 const Board = () => {
   const dispatch = useDispatch()
-  const [grid,setGrid]= useState([])
+  // const [grid,setGrid]= useState([])
   const game = useSelector((state) => state.dashboard);
   const clickLocation = useSelector((state)=> state.dashboard.ClickLocation);
   // properties of cell
   const properites = {
     revealed: false,
     flagged: false,
-    value: '0'
+    value: 0
   }
 
   // based on difficulty - we decide the size of our matrix
   // for easy 9*9. for medium 12*12. for hard 16*16
-
   const {size,flagsTotal, bombTotal,gameStarted} = game;
+
+
   /* initalize the board to size base on difficulty */
-  var matrix = [];
-  for (var i = 0; i < size; i++) {
-    matrix[i] = new Array(size);
-  }
-  /* create empty matrix */
-  for(var i=0;i<size;i++){
-    for(var j=0;j<size;j++){
-      matrix[i][j] = properites;
+  const initialMatrix = (() => {
+    const matrix = [];
+    for (let i = 0; i < size; i++) {
+      matrix[i] = new Array(size);
     }
+  
+    // Create empty matrix with properties
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        matrix[i][j] = properites; // Replace 'properties' with your desired value
+      }
+    }
+    return matrix;
+  })();
+  
+  function matrixReducer(state, action) {
+    switch (action.type) {
+      case 'SET_MATRIX':
+        return action.matrix;
+      case 'SET_FLAG':
+        const {x,y,flagged} = action.payload;
+        const updatedMatrix = [...state] // get exist matrix
+        updatedMatrix[x][y] ={
+          ...updatedMatrix[x][y],
+          flagged:flagged // set flagged based on action.payload
+        }
+          
+      default:
+        return state;
+    }
+  }
+  const [matrix, dispatchMatrix] = useReducer(matrixReducer, initialMatrix);
+
+  const setvalInCell = (x,y,val) =>{
+    const updatedMatrix = [...matrix]
+    updatedMatrix[x][y]={
+      ...matrix[x][y],
+      value: val
+    }
+    dispatchMatrix({type:'SET_MATRIX',matrix:updatedMatrix})
+  }
+  const SetRevealCell = (matrix,x,y)=>{
+    matrix[x][y]={
+     ...matrix[x][y],
+     revealed: true
+   }
   }
 /* placing bombs on the board */
 /*we don't put a bomb where the user click and all his 8 neighbors!*/
@@ -38,13 +76,9 @@ const PlacingBombs = (clickLocation) =>{
     while(CurrentBombs > 0){
       x = Math.floor(Math.random()*(size-1))
       y = Math.floor(Math.random()*(size-1))
-
        if(matrix[x][y].value != 'x' && !NotAllowToHaveBomb(clickLocation[0],clickLocation[1],x,y) ){
         // put a bomb at random location on board if not exist
-         matrix[x][y]={
-          ...matrix[x][y],
-          value: 'x'
-         }
+        setvalInCell(x,y,'x')
         // reduce number of bombs in 1
          CurrentBombs--
      }
@@ -64,18 +98,14 @@ const UpdateMatBasedOnBombs =()=>{
   for(var i=0;i<size;i++){
     for(var j=0;j<size;j++){
       // forEach on all my neighbors and count how many bombs we have around me
-      if(matrix[i][j].value != 'x' ) 
-      matrix[i][j]={
-        ...matrix[i][j],
-        value: countNeighborsBomb(i,j)
-       }
+      if(matrix[i][j].value != 'x') 
+        setvalInCell(i,j,countNeighborsBomb(i,j))
     }
   }
-  setGrid(matrix)
 }
 /* the function return true if the given [x,y] location is a bomb */
 const IsYourNeighborBomb = ({x,y})=>{
-  if(x >= 0 && x< size && y >= 0 && y < size){
+  if(x >= 0 && x < size && y >= 0 && y < size){
     if(matrix[x][y].value == 'x') 
       return true;
   }
@@ -91,44 +121,38 @@ const countNeighborsBomb=(x,y)=>{
   for(let xAxis=-1;xAxis<=1;xAxis++){
     for(let yAxis=-1;yAxis<=1;yAxis++){
       if(xAxis == 0 && yAxis == 0) continue;
-      // count bombs around me
-      if(IsYourNeighborBomb({x:x+xAxis,y:y+yAxis})) neighborsBomb++;
+      if(IsYourNeighborBomb({x:x+xAxis,y:y+yAxis})) neighborsBomb++; // count bombs around me
     }
   }
   return neighborsBomb;
 }
 
-const RevealCells =() =>{
-  FindZeroNeighbor( clickLocation[0], clickLocation[1])
-}
-
 const FindBorderNeighbor = (x,y)=>{
+  const updatedMatrix = [...matrix]
   for(let xAxis=-1;xAxis<=1;xAxis++){
     for(let yAxis=-1;yAxis<=1;yAxis++){
       if(isValidIndex(x,y,xAxis,yAxis)){
-        if(matrix[x][y].value == 0){
-          matrix[x+xAxis][y+yAxis].revealed = true;
+        if(matrix[x][y].value === 0) {
+          SetRevealCell(updatedMatrix,x+xAxis,y+yAxis)
         }
      }
     }
   }
-  
+  dispatchMatrix({type:'SET_MATRIX',matrix:updatedMatrix})
 }
 /*Flood fill algorithm */
-const FindZeroNeighbor = (x, y) => {
-  if (matrix[x][y].value !== 0 || matrix[x][y].revealed) {
-    return;
-  }
+const RevealCells = (x, y) => {
+  if (matrix[x][y].value !== 0 || matrix[x][y].revealed) return;
   for (let xAxis = -1; xAxis <= 1; xAxis++) {
     for (let yAxis = -1; yAxis <= 1; yAxis++) {
-      if (isValidIndex(x,y,xAxis,yAxis)) {
-          matrix[x][y].revealed = true;
-          FindZeroNeighbor(x + xAxis, y + yAxis);
-        }
+      if (isValidIndex(x,y,xAxis,yAxis)){
+          SetRevealCell(matrix,x, y)
+         RevealCells(x + xAxis, y + yAxis);
+      }
     }
   }
    // reveal his neighbors - create for us a border of cells around the 0's cells
-   FindBorderNeighbor(x,y);
+    FindBorderNeighbor(x,y);
 }
 
 const isValidIndex = (x,y,xAxis,yAxis)=>{
@@ -137,10 +161,8 @@ const isValidIndex = (x,y,xAxis,yAxis)=>{
 
 useEffect(() => {
   if(clickLocation[0] != -1 && clickLocation[1] != -1){
-    if(!gameStarted){
-      PlacingBombs(clickLocation) // set matrix
-    }
-    RevealCells()
+    if(!gameStarted) PlacingBombs(clickLocation) // set matrix
+    RevealCells(clickLocation[0], clickLocation[1])
   }
 }, [clickLocation])
 
@@ -150,10 +172,12 @@ useEffect(() => {
         <div key={`row-${rowIndex} row`}>
           {row.map((element, columnIndex) => (
             <Cell
+            key={`${rowIndex}-${columnIndex}`}
             locationX={rowIndex}
             locationY={columnIndex} 
-            grid={grid}
-            key={`${rowIndex}-${columnIndex}`} />
+            matrix = {matrix}
+            dispatchMatrix={dispatchMatrix}
+             />
           ))}
         </div>
       ))}
--- src/components/DashBoard/Cell/Cell.jsx
+++ src/components/DashBoard/Cell/Cell.jsx
@@ -2,44 +2,40 @@ import React,{useEffect,useRef} from 'react'
 import { useDispatch } from 'react-redux'
 import {images} from '../../../constants/index'
 import { setclickedLocation,setFlagsInUse } from '../../../features/DashBoard/DashBoardSlice'
-const Cell = ({locationX,locationY,grid}) => {
+const Cell = ({locationX,locationY,matrix,dispatchMatrix}) => {
   const dispatch = useDispatch()  
   const Ref = useRef()
 
   useEffect(() => {
-    if(grid.length > 0){
-      if(grid[locationX][locationY].revealed){
+    if(matrix.length > 0){
+      if(matrix[locationX][locationY].revealed){
         Ref.current.classList.remove(`cell-${locationX}-${locationY}`)
         Ref.current.classList.add(`cellActive-${locationX}-${locationY}`)
       }
     }
-  }, [grid])
+  }, [matrix])
   
 
-  const placeFlag = ()=>{
-    // update in matrix 
+  const SetFlag = (e) =>{
+    e.preventDefault() // prevent default on right click 
+    dispatchMatrix(
+      {type:'SET_FLAG',
+      payload: {
+        x: locationX,
+        y : locationY,
+        flagged:!matrix[locationX,locationY].flagged
+      }})
     dispatch(setFlagsInUse()) // increase flagsInUse +1
-    if(!grid[locationX][locationY].revealed)
-      grid[locationX][locationY].value = 'f'
-
-
   }
   return (
     <div className={`cell cell-${locationX}-${locationY}`} ref={Ref} 
-    onContextMenu = {(e)=>{
-      e.preventDefault()
-      placeFlag()}}
+    onContextMenu = {SetFlag}
     onClick={(e)=>{
       let location = [locationX,locationY]
-      dispatch(setclickedLocation(location))
+       dispatch(setclickedLocation(location))
       }}>
-    {grid.length > 0 &&  grid[locationX][locationY].revealed ? 
-    (grid[locationX][locationY].value  != 0 ) ?
-      grid[locationX][locationY].value 
-      :'' 
-    : (grid[locationX][locationY].value == 'f')?
-      <img src={images.flag}/> :
-      ''
+    {matrix.length > 0 &&  matrix[locationX][locationY].revealed ? ((matrix[locationX][locationY].value  != 0 ) ? matrix[locationX][locationY].value :'') 
+    : (matrix.length > 0 && matrix[locationX][locationY].flagged)? <img src={images.flag}/> : ''
     }
     </div>
   )
